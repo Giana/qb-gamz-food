b42a41f17ff837df6c514e83f6cf11528004929b
Giana <giana@giana.dev>
2023-05-24 17:10:26 -0500

    Upgrade to fxmanifest

---
+++ gamz-food/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'Giana - github.com/Giana (Original author: github.com/gamziboi)'
+description 'qb-gamz-food (Forked from github.com/gamziboi/gamz-food)'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_script {
+    'client/main.lua',
+    'client/utils.lua'
+}
+
+server_script {
+    'server/main.lua'
+}
+
+lua54 'yes'
